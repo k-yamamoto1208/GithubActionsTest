d015860608bad7a7d0485d55d1e26d2c7214b245
Kohei <>
2022-05-29 21:34:08 +0900

    テスと

--- app/src/main/java/com/example/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/example/githubactionstest/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
 
 
         val aiueo="こんにちは"
